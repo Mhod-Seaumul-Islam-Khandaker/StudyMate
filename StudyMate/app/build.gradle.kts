f118a868cd530177b3bfc5bbab7884060c9bb0b9
Seaum <seaumul.islam.khandaker@g.bracu.ac.bd>
2025-12-19 08:15:42 +0600

    Successfully removed firebase and added superbase

--- StudyMate/app/build.gradle.kts
+++ StudyMate/app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    // The Google Services plugin needs to be applied in the application module
-    id("com.google.gms.google-services") version "4.4.4" // Ensure you use the latest version here
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -15,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.studymate"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -46,8 +45,6 @@ android {
 }
 
 dependencies {
-    // Import the Firebase BoM FIRST to manage all versions automatically
-    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
 
     // AndroidX Core KTX and UI components
     implementation(libs.androidx.core.ktx)
@@ -59,15 +56,16 @@ dependencies {
     // Explicit dependencies with hardcoded versions (outside of BoM management)
     implementation("androidx.cardview:cardview:1.0.0")
 
-    // Firebase dependencies (version managed by the BoM)
-    // Use the main 'firebase-auth' module (KTX features are now here)
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-analytics")
-
     // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.11.0")
+    // superbase
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.ktor:ktor-client-android:3.3.3")
+
 
 }
