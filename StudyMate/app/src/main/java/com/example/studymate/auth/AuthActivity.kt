dfe8310f4cd6e770ee1cf164dcd5631b9af34896
Seaum <seaumul.islam.khandaker@g.bracu.ac.bd>
2025-12-27 17:20:13 +0600

    Accessibility added to sign up and login screen

--- StudyMate/app/src/main/java/com/example/studymate/auth/AuthActivity.kt
+++ StudyMate/app/src/main/java/com/example/studymate/auth/AuthActivity.kt
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.studymate.R
 import com.example.studymate.databinding.ActivityAuthBinding
+import com.example.studymate.utils.TextToSpeechHelper
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
+
+    @Inject
+    lateinit var ttsHelper: TextToSpeechHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,24 +27,28 @@ class AuthActivity : AppCompatActivity() {
 
         // Start with LoginFragment by default
         if (savedInstanceState == null) {
-            loadFragment(LoginFragment())
+            loadFragment(LoginFragment(), "Login Screen")
         }
     }
 
     /**
      * Load a fragment into the authFragmentContainer
      */
-    fun loadFragment(fragment: Fragment) {
+    fun loadFragment(fragment: Fragment, screenName: String = "") {
         supportFragmentManager.beginTransaction()
             .replace(R.id.authFragmentContainer, fragment)
             .commit()
+            
+        if (screenName.isNotEmpty()) {
+            ttsHelper.speak("Navigating to $screenName")
+        }
     }
 
     /**
      * Switch to signup fragment
      */
     fun switchToSignup() {
-        loadFragment(SignupFragment())
+        loadFragment(SignupFragment(), "Create Account Screen")
         supportActionBar?.title = "Create Account"
     }
 
@@ -47,7 +56,14 @@ class AuthActivity : AppCompatActivity() {
      * Switch to login fragment
      */
     fun switchToLogin() {
-        loadFragment(LoginFragment())
+        loadFragment(LoginFragment(), "Login Screen")
         supportActionBar?.title = "Welcome Back"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (isFinishing) {
+            ttsHelper.shutdown()
+        }
     }
 }
